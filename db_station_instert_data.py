fac3149c13284f34a731eda4aa3c841ed2cb4879
Piotr Dzieszkowski <piotr.dzieszkowski@gmail.com>
2022-05-16 16:05:54 +0100

    sqlalchemy

--- db_station_instert_data.py
+++ db_station_instert_data.py
@@ -2,12 +2,11 @@ from db_station_create_table import stations, measure, engine
 import csv
 
 
-
 def load_clean_stations_csv():
-    data =[]
-    with open("clean_stations.csv") as csvfile:
-        readCSV =csv.read(csvfile,delimiter = ",")
-        for i in readCSV:
+    with open("clean_stations.csv", newline="") as csvfile:
+        data=[]
+        csvreader = csv.DictReader(csvfile)
+        for i in csvreader:
             data.append ({"station" : i["station"], "latitude" : (i["latitude"]), "longitude" : (i["longitude"]), "elevation" : (i["elevation"]), "name" : i["name"], "country" : i["country"], "state" : i["state"]})
     return data
 
